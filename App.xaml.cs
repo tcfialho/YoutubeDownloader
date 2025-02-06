e400c565ac8b6492fe20cd591e39851b5c3d75cb
Thiago F. <tcfialho@gmail.com>
2025-02-05 22:03:55 -0300

    refactor: improve application initialization and window creation

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,10 +2,19 @@
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
+    public App()
+    {
+        InitializeComponent();
+    }
 
-		MainPage = new AppShell();
-	}
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var window = new Window(new AppShell());
+        
+        // Você pode configurar a janela aqui se necessário
+        // window.Width = 800;
+        // window.Height = 600;
+        
+        return window;
+    }
 }
